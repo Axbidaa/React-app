4b4b1b9187379255e0746f464f263c17675d78ea
Axbidaa <nezumrashid@gmail.com>
2024-10-05 10:34:27 +0400

    Updated the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,14 +32,24 @@ function App() {
 
   return (
     <div className="App">
-      <div className="weather-card">
+      <div className="title fade-in-title">
+        My Weather App
+      </div>
+      <div className="welcome-message fade-in-content">
+        <p>Welcome to my React application. This is a simple landing page of a weather widget app where users
+          are able to see the relevant information about the weather. The application has two parts: the backend
+          and the frontend. The backend is responsible for creating two api endpoints; weather info and
+          motivational quote while the frontend is responsible for retrieving the information from the
+          created endpoints.</p>
+      </div>
+      <div className="weather-card fade-in-content">
         <h2>Weather in {weather.city}</h2>
-        <h3>{weather.temperature}</h3>
+        <h3 className="temperature">{weather.temperature}</h3>
         <p>{weather.description}</p>
         <p>Humidity: {weather.humidity}</p>
         <p>Wind Speed: {weather.wind_speed}</p>
       </div>
-      <div className="quote-card">
+      <div className="quote-card fade-in-content">
         <h3>Motivational Quote</h3>
         <p>"{quote.quote}"</p>
         <p>- {quote.author}</p>
